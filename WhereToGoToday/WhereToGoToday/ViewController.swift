0cbd0a3ef3aeeb7ad254603d511ccbec0e81ba14
EllaLiu <ella91714@gmail.com>
2024-08-07 19:59:33 +0800

    初建專案

---
+++ WhereToGoToday/WhereToGoToday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhereToGoToday
+//
+//  Created by Gorgais Yeh on 2024/8/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello world")
+    }
+
+
+}
+
